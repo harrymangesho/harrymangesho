e8e35c0f6ef4ca1ba6d27efe4016e61342ab974b
harrymangesho <hmangesho08@gmail.com>
2024-12-27 13:17:47 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-// This is an optional JavaScript file
-window.onload = function() {
-    alert("Welcome to my simple website!");
-};
+// JavaScript to handle form validations or dynamic interactions can be added here
+document.querySelector('form').addEventListener('submit', function(e) {
+    let donationAmount = document.querySelector('input[name="amount"]:checked');
+    if (!donationAmount) {
+        alert("Please select a donation amount.");
+        e.preventDefault(); // Prevent form submission if no amount is selected
+    }
+});
